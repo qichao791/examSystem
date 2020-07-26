3ef3ea0e37bc9a7356b374cb17119e00b9c8fe76
41612196 <1185391863@qq.com>
2020-07-26 19:41:56 +0800

    7-26 pwj app

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require('body-parser')
-
+const departRouter = require("./routes/departRoutes");
+const branchRouter = require("./routes/branchRoutes");
+const userRouter = require("./routes/userRoutes");
+const paperRouter = require("./routes/paperRoutes");
+const questionbankRouter = require("./routes/questionbankRoutes");
+const subpublicbankRouter = require("./routes/subpublicbankRoutes");
+const professionalbankRouter = require("./routes/professionalbankRoutes");
+const userpaperRouter = require("./routes/userpaperRoutes");
+const adminRouter = require("./routes/adminRoutes");
 const scoreRouter = require("./routes/scoreRoutes");
 
 
@@ -18,7 +26,15 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/departs", departRouter);
+app.use("/api/v1/branches", branchRouter);
+app.use("/api/v1/publicquestion", questionbankRouter);
+app.use("/api/v1/subpublicquestion", subpublicbankRouter);
+app.use("/api/v1/professionalquestion", professionalbankRouter);
+app.use("/api/v1/userpaper", userpaperRouter);
+app.use("/api/v1/paper", paperRouter);
 app.use("/api/v1/score", scoreRouter);
 
 module.exports = app;
