d1d71b9ab88a05de415afae87a71922e0935c0d0
41612196 <1185391863@qq.com>
2020-08-09 14:59:15 +0800

    8-9 pwj 4

--- controller/questionbankController.js
+++ controller/questionbankController.js
@@ -28,11 +28,11 @@ exports.getLikeQuestion = async (req, res) => {
 exports.getLikeQuestion = async (req, res) => {
   try {
     //const stem = await PublicQues.find({'statement.stem': new RegExp(req.body.keywords)});
-    const reg = new RegExp(req.body.stem, 'g');
+    const reg = new RegExp(req.body.stem, "g");
     //RegExp对象表示正则表达式，它可以对字符串执行模式匹配，‘g’表示执行全局配置
     const result = await PublicQues.find({
-      'statement.stem': { $regex: reg } //$regex用于实现模糊查询
-    })
+      "statement.stem": { $regex: reg } //$regex用于实现模糊查询
+    });
     res.status(200).json({
       status: "success",
       result
@@ -40,14 +40,14 @@ exports.getLikeQuestion = async (req, res) => {
   } catch (err) {
     res.status(404).json({ status: "fail", message: err });
   }
-}
+};
 
 exports.getAllPublicQues = async (req, res) => {
   try {
     const data = await PublicQues.find();
     res.status(200).json({
       status: "success",
-      data,
+      data
     });
   } catch (err) {
     res.status(404).json({ status: "fail", message: err });
@@ -61,13 +61,13 @@ exports.getPublicQuesByGrade = async (req, res) => {
     } else {
       data = await PublicQues.aggregate([
         { $match: { grade: req.body.grade } },
-        { $sample: { size: req.body.amount } },
+        { $sample: { size: req.body.amount } }
         //{$project:{ _id:0,statement:1 }}
       ]);
     }
     res.status(200).json({
       status: "success",
-      data,
+      data
     });
   } catch (err) {
     res.status(404).json({ status: "fail", message: err });
@@ -78,19 +78,20 @@ exports.createPublicQues = async (req, res) => {
   try {
     const newQues = await PublicQues.create(req.body);
     res.send(newQues);
-
   } catch (err) {
     res.status(404).json({ status: "fail", message: err });
   }
 };
 exports.deletePublicQues = async (req, res) => {
   try {
-    const readyToDeleteQues = await PublicQues.findOneAndDelete({ _id: req.params.ques_id });
+    const readyToDeleteQues = await PublicQues.findOneAndDelete({
+      _id: req.params.ques_id
+    });
 
     if (readyToDeleteQues != null) {
       res.status(204).json({
         status: "success",
-        data: null,
+        data: null
       });
     } else {
       res.status(404).json({ status: "fail", message: "not found" });
@@ -101,14 +102,18 @@ exports.deletePublicQues = async (req, res) => {
 };
 exports.updatePublicQues = async (req, res) => {
   try {
-    const data = await PublicQues.findOneAndUpdate({ _id: req.params.ques_id }, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const data = await PublicQues.findOneAndUpdate(
+      { _id: req.params.ques_id },
+      req.body,
+      {
+        new: true,
+        runValidators: true
+      }
+    );
 
     res.status(200).json({
       status: "success",
-      data,
+      data
     });
   } catch (err) {
     res.status(404).json({ status: "fail", message: err });
@@ -128,59 +133,47 @@ exports.importQuessToPublicBank = async (req, res) => {
 
   try {
     let data = req.body;
-<<<<<<< HEAD
-    console.log("piblicBankData:",data)
-    for(let i=0;i<data.length;i++){
-=======
     for (let i = 0; i < data.length; i++) {
->>>>>>> 465d6146550ecaa1b9956edd6821f124a45d2c67
       let ques = new PublicQues();
       ques.statement = {
         stem: data[i].stem,
-        options: data[i].options.split('$'),
-        right_answer: data[i].right_answer,
-      }
+        options: data[i].options.split("$"),
+        right_answer: data[i].right_answer
+      };
       ques.analysis = data[i].analysis;
       ques.knowlege = data[i].knowlege;
       ques.grade = data[i].grade;
 
       var images, voices, videos;
-      if (data[i].images == null)
-        images = [];
-      else
-        images = data[i].images.split('$')
-      if (data[i].voices == null)
-        voices = [];
-      else
-        voices = data[i].voices.split('$')
-      if (data[i].videos == null)
-        videos = [];
-      else
-        videos = data[i].videos.split('$')
+      if (data[i].images == null) images = [];
+      else images = data[i].images.split("$");
+      if (data[i].voices == null) voices = [];
+      else voices = data[i].voices.split("$");
+      if (data[i].videos == null) videos = [];
+      else videos = data[i].videos.split("$");
       ques.attachment = {
         image: images,
         voice: voices,
-        video: videos,
-
-      }
+        video: videos
+      };
       await ques.save();
     }
     res.status(200).json({
-      status: "success",
+      status: "success"
     });
   } catch (err) {
     res.status(404).json({ status: "fail", message: err });
   }
-}
+};
 
 exports.getStatementByKeyWords = async (req, res) => {
   try {
     //const stem = await PublicQues.find({'statement.stem': new RegExp(req.body.keywords)});
-    const stem = new RegExp(req.body.keywords, 'g');
+    const stem = new RegExp(req.body.keywords, "g");
     //RegExp对象表示正则表达式，它可以对字符串执行模式匹配，‘g’表示执行全局配置
     const result = await PublicQues.find({
-      'statement.stem': { $regex: stem } //$regex用于实现模糊查询
-    })
+      "statement.stem": { $regex: stem } //$regex用于实现模糊查询
+    });
     res.status(200).json({
       status: "success",
       result
@@ -188,4 +181,4 @@ exports.getStatementByKeyWords = async (req, res) => {
   } catch (err) {
     res.status(404).json({ status: "fail", message: err });
   }
-}
+};
