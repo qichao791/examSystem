37bbe11390ec55c7ff8dfc8959d2dcf2be548c4e
41612196 <1185391863@qq.com>
2020-09-07 17:17:28 +0800

    9-7 pwj

--- controller/professionalbankController.js
+++ controller/professionalbankController.js
@@ -115,41 +115,46 @@ exports.updateProfQues = async (req, res) => {
 exports.importQuessToProfessionalBank = async(req,res) =>{ 
   try{
     let data = req.body;
-    //console.log("professionalBankData:",data)
+    // console.log("professionalBankData:",data)
       for(let i=0,j=0;i<data.length;i++){
         let ques = new ProfQues();  
 
         let departId = await Depart.findOne({ depart_name:data[i].depart_name},'_id');
-        let branchId_list = await Branch.find({ branch_name:data[i].branch_name},'_id');
+        let branchId = await Branch.findOne({ branch_name:data[i].branch_name},'_id');
         ques.depart_id = departId._id;
-        for(j=0;j<branchId_list.length;j++){ 
-          const br = await Branch.aggregate(
-            [
-            {
-              $lookup: {
-                from: "department", //the colletion named department in the database qc of mongodb
-                localField: "_id", //the field of the collection branch which also is the model Branch in mongoose
-                foreignField: "branches", //the field of the collection department
-                as: "belongedToDepart",
-              },
-            },
-            {
-              $match:{_id:branchId_list[j]._id}
-            },
-            {
-              $project: {
-                _id: 0,
-                "belongedToDepart._id": 1,
-              },
-            },
-          ]);
-        
-          if(br[0].belongedToDepart[0]._id==ques.depart_id){
-              break;
-          }
+        // console.log("branchId_list",branchId_list)
+        // for(j=0;j<branchId_list.length;j++){ 
+        //   const br = await Branch.aggregate(
+        //     [
+        //     {
+        //       $lookup: {
+        //         from: "department", //the colletion named department in the database qc of mongodb
+        //         localField: "_id", //the field of the collection branch which also is the model Branch in mongoose
+        //         foreignField: "branches", //the field of the collection department
+        //         as: "belongedToDepart",
+        //       },
+        //     },
+        //     {
+        //       $match:{_id:branchId_list[j]._id}
+        //     },
+        //     {
+        //       $project: {
+        //         _id: 0,
+        //         "belongedToDepart._id": 1,
+        //       },
+        //     },
+        //   ]);
+        //   console.log("br",br)
+        //   console.log("br[0].belongedToDepart[0]",br[0].belongedToDepart[0])
+        //   if(br[0].belongedToDepart[0]._id==ques.depart_id){
+        //       break;
+        //   }
               
-        }
-        ques.branch_id = branchId_list[j]._id;
+        // }
+        // console.log("j",j)
+        // console.log("branchId_list[j]",branchId_list[j])
+        ques.branch_id = branchId._id;
+       
         ques.statement = {
             stem: data[i].stem,
             options: data[i].options.split('$'),
